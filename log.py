1697a7ff1d8a0489c9c541c74c7dae46c381a66e
16pa1a0594 <38848801+16pa1a0594@users.noreply.github.com>
2018-07-07 21:59:40 +0530

    Update log.py

--- log.py
+++ log.py
@@ -1,80 +1,78 @@
-#!/usr/bin/env python3
-
 import psycopg2
-
-# Database queries
-# Database query 1: What are the three most popular articles of all time?
-request_articles = """select articles.title, count(*) as num
-                 FROM articles
-                JOIN log
-                ON log.path LIKE concat('/article/%', articles.slug)
-                group by articles.title
-                order by num desc
-                limit 3;"""
-
-# Database query 2: Who are the most popular article authors of all time?
-request_authors = """select authors.name, count(*) as num
-                FROM authors
-                JOIN articles
-                ON authors.id = articles.author
-                JOIN log
-                ON log.path like concat('/article/%', articles.slug)
-                group by authors.name
-                order by num desc;
-                """
-
-# Database query 3: On which day did more than 1% of requests lead to errors?
-request_errors = """select time, percentagefailed
-                from percentagecount
-                where percentagefailed > 1;
-                """
+#importing for Proper date format
+from datetime import datetime
 
 
-# Query data from the database, open and close the connection
-def query_db(sql_request):
-        conn = psycopg2.connect(database="news")
-        cursor = conn.cursor()
-        cursor.execute(sql_request)
-        results = cursor.fetchall()
-        conn.close()
-        return results
+# What are the most popular of three articles of all time?
+data1 = ("What are the most popular of three articles of all time ?")
+query1 = (
+    "select articles.title, count(*) as views "
+    "from articles inner join log on log.path "
+    "like concat('%', articles.slug, '%') "
+    "where log.status like '%200%' group by "
+    "articles.title, log.path order by views desc limit 3")
+
+# Who are the most popular of article authors of all time?
+data2 = ("Who are the most popular of article authors of all time?")
+query2 = (
+    "select authors.name, count(*) as views from articles inner "
+    "join authors on articles.author = authors.id inner join log "
+    "on log.path like concat('%', articles.slug, '%') where "
+    "log.status like '%200%' group "
+    "by authors.name order by views desc")
+
+# which days did more than 1% of requests lead to errors
+data3 = ("which days did more than 1% of requests lead to errors?")
+query3= (
+    "select day, perc from ("
+    "select day, round((sum(requests)/(select count(*) from log where "
+    "substring(cast(log.time as text), 0, 11) = day) * 100), 2) as "
+    "perc from (select substring(cast(log.time as text), 0, 11) as day, "
+    "count(*) as requests from log where status like '%404%' group by day)"
+    "as log_percentage group by day order by perc desc) as final_query "
+    "where perc >= 1")
 
 
-# Writing the report
-# Print a title of the report
-def print_title(title):
-    print ("\n\t\t" + title + "\n")
+def connect(db_name="news"):
+    """Connect or check database connection"""
+    try:
+        database = psycopg2.connect("dbname={}".format(db_name))
+        cur = database.cursor()
+        return database, cur
+    except:
+        print ("Unable to connect to the database")
 
 
-# Print the top three articles of all time
-def top_three_articles():
-    top_three_articles = query_db(request_articles)
-    print_title("Top 3 articles of all time")
-
-    for title, num in top_three_articles:
-        print(" \"{}\" -- {} views".format(title, num))
-
-
-# Print the top authors of all time
-    def top_three_authors():
-        top_three_authors = query_db(request_authors)
-        print_title("Top authors of all time")
-
-    for name, num in top_three_authors:
-        print(" {} -- {} views".format(name, num))
+def get_query_result(query):
+    #Return results
+    database, cur = connect()
+    cur.execute(query)
+    return cur.fetchall()
+    database.close()
 
 
-# Print the days in which there were more than 1% bad requests
-def high_error_days():
-    high_error_days = query_db(request_errors)
-    print_title("Days with more than one percentage of bad requests")
+def print_query_result(query_result):
+    print (query_result[1])
+    for index, results in enumerate(query_result[0]):
+        print ("\t"+str(index+1)+"."+str(results[0])+" - "+str(results[1])+" views")
 
-    for day, percentagefailed in high_error_days:
-        print("""{0:%B %d, %Y}
-            -- {1:.2f} % errors""".format(day, percentagefailed))
+
+def print_error_result(query_result):
+    print (query_result[1])
+    for results in query_result[0]:
+        d= results[0]
+        date_obj = datetime.strptime(d, "%Y-%m-%d")
+        formatted_date = datetime.strftime(date_obj, "%B %d, %Y")        
+        print ("\t"+str(formatted_date)+" - "+str(results[1]) + "% errors")
+
 
 if __name__ == '__main__':
-    top_three_articles()
-    top_three_authors()
-    high_error_days()
+    # get results
+    popular3_articles = get_query_result(query1), title1
+    popular4_authors = get_query_result(query2), title2
+    error_days = get_query_result(query3), title3
 
+    # print results
+    print_query_result(popular3_articles)
+    print_query_result(popular4_authors)
+    print_error_result(error_days)
